4f5fdc9c257e6dd20ff13c0e3425572f180f0e3c
george746 <74596104+george746@users.noreply.github.com>
2022-02-11 17:26:46 +0100

    Update of the JDBC connection and settings

--- Personnel/src/jdbc/JDBC.java
+++ Personnel/src/jdbc/JDBC.java
@@ -28,6 +28,7 @@ public class JDBC implements Passerelle
 		{
 			System.out.println(e);
 		}
+		System.out.println("bonjour");
 	}
 	
 	@Override
@@ -74,7 +75,7 @@ public class JDBC implements Passerelle
 		try 
 		{
 			PreparedStatement instruction;
-			instruction = connection.prepareStatement("insert into ligue (nom) values(?)", Statement.RETURN_GENERATED_KEYS);
+			instruction = connection.prepareStatement("insert into ligue (nomLigue) values(?)", Statement.RETURN_GENERATED_KEYS);
 			instruction.setString(1, ligue.getNom());		
 			instruction.executeUpdate();
 			ResultSet id = instruction.getGeneratedKeys();
