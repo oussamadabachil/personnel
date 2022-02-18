3b38b3e8bf744e5dbca5264e5c9ae43f15a37704
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-02-18 10:23:46 +0100

    Adding the SQL DBB file.

---
+++ Personnel/docs/bdd.sql
@@ -0,0 +1,20 @@
+CREATE TABLE EMPLOYE (
+  idEmploye INT NOT NULL AUTO_INCREMENT,
+  nomEmploye VARCHAR(42),
+  prenomEmploye VARCHAR(42),
+  mailEmploye VARCHAR(42),
+  mdpEmploye VARCHAR(42),
+  dateArrivee DATE,
+  dateDepart DATE,
+  typeEmploye TINYINT,
+  idLigue INT,
+  PRIMARY KEY (idEmploye)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE LIGUE (
+  idLigue INT NOT NULL AUTO_INCREMENT,
+  nomLigue VARCHAR(42),
+  PRIMARY KEY (idLigue)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE EMPLOYE ADD FOREIGN KEY (idLigue) REFERENCES LIGUE (idLigue);
